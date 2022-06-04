083b2ef543ea7452598893685807f7a5db2cbdb8
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-06-04 15:20:49 +0200

    [exercice]
    trained same exercices again

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import {
   useObservable,
   useObservableCallback,
-  useObservableState,
   useSubscription,
 } from "observable-hooks";
 import React from "react";
 import {
+  combineLatest,
   concat,
-  debounceTime,
-  defer,
+  concatAll,
+  concatMap,
+  count,
   delay,
-  distinctUntilChanged,
   EMPTY,
+  endWith,
   filter,
+  from,
   interval,
   map,
-  merge,
   mergeMap,
   of,
-  pluck,
   scan,
   share,
   startWith,
-  switchAll,
   switchMap,
+  take,
+  takeUntil,
   tap,
   timer,
+  withLatestFrom,
 } from "rxjs";
 import styled from "styled-components";
 const Container = styled.div`
@@ -48,7 +50,7 @@ const Button = styled.button`
   cursor: pointer;
   display: block;
   color: ${(props) => props.color};
-  background-color: black;
+  background-color: #00000058;
   text-transform: uppercase;
   &:hover {
     color: ${(props) => props.yellow};
@@ -62,44 +64,39 @@ const Input = styled.input`
   outline: none;
   font-size: 1rem;
 `;
-
+const Figure = styled.div`
+  border: 1px solid red;
+  width: 90vw;
+  margin: 20px auto;
+`;
+const Progress = styled.div`
+  width: ${(props) => props.width}%;
+  background-color: ${(props) => (props.width === 100 ? "green" : "aliceblue")};
+  height: 21px;
+  transition: all 500ms ease;
+`;
 export default function App() {
-  const [isSaving, setIsSaving] = React.useState("All changes saved");
-  const saveChange = (v) => of(v).pipe(delay(1500));
-  const [handleChange, input$] = useObservableCallback((e$) =>
-    e$.pipe(
-      debounceTime(500),
-      pluck("target", "value"),
-      distinctUntilChanged(),
-      share()
+  const [message, setMessage] = React.useState(null);
+  const source$ = useObservable(() =>
+    interval(1000).pipe(filter((v) => v % 2 === 0))
+  );
+  const evenNumberCount$ = useObservable(() =>
+    source$.pipe(scan((acc) => acc + 1, 0))
+  );
+  const firstFiveEvenNum$ = useObservable(() =>
+    evenNumberCount$.pipe(filter((v) => v > 1))
+  );
+  const combined$ = useObservable(() =>
+    source$.pipe(
+      withLatestFrom(evenNumberCount$),
+      map(([value, count]) => `Count: ${count} value : ${value}`),
+      takeUntil(firstFiveEvenNum$),
+      endWith("Finished")
     )
   );
-  const saving$ = useObservable(() =>
-    input$.pipe(
-      map(() => of("Saving")),
-      tap(() => setIsSaving(true))
-    )
-  );
-  const saved$ = useObservable(() =>
-    input$.pipe(
-      mergeMap(saveChange),
-      tap(() => setIsSaving(false)),
-      filter(() => !isSaving),
-      map(() =>
-        concat(of("Saved!"), timer(3000)).pipe(
-          map(() => `Last updated: ${Date.now()}`)
-        )
-      )
-    )
-  );
-  const merged$ = useObservable(() => merge(saving$, saved$).pipe(switchAll()));
-  const msg = useObservableState(merged$, "All changes saved");
-  React.useEffect(() => {}, []);
-  return (
-    <Container>
-      <Displayer>Take a note!</Displayer>
-      <Input onChange={handleChange} />
-      <Displayer>{msg}</Displayer>
-    </Container>
-  );
+  useSubscription(combined$, (v) => {
+    if (v === "Finished") setMessage(v);
+    else console.log(v);
+  });
+  return <Container>{message}</Container>;
 }
